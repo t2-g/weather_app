8b3849caacfa9c297097b29d68b8ea741a560ee9
t2-g <89742029+t2-g@users.noreply.github.com>
2022-07-20 21:43:49 +0530

    init

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // 007e5acd804e8400e57c455fd7d22383
 
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-const weatheraApi={
+const weatherApi={
     key: "007e5acd804e8400e57c455fd7d22383",
-    baseUrl: "https://api.openweathermap.org/data/2.5/weather?"
+    baseUrl: "http://api.openweathermap.org/data/2.5/weather?"
 }
 
 //Analogous functions 
@@ -27,5 +27,62 @@ function getWeatherReport(city) {
 }
 
 //show weather eport
+function showWeatherReport(weather){
+    console.log(weather);
 
+    let city = document.getElementById('city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max');
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate);
+
+    
+    if(weatherType.textContent == 'Clear') {
+        document.body.style.backgroundImage = "url('images/clear.jpg')";
+        
+    } else if(weatherType.textContent == 'Clouds') {
+
+        document.body.style.backgroundImage = "url('images/cloud.jpg')";
+        
+    } else if(weatherType.textContent == 'Haze') {
+
+        document.body.style.backgroundImage = "url('images/cloud.jpg')";
+        
+    }     else if(weatherType.textContent == 'Rain') {
+        
+        document.body.style.backgroundImage = "url('images/rain.jpg')";
+        
+    } else if(weatherType.textContent == 'Snow') {
+        
+        document.body.style.backgroundImage = "url('images/snow.jpg')";
+    
+    } else if(weatherType.textContent == 'Thunderstorm') {
+    
+        document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+        
+    } 
+}
 //date manage
+function dateManage(dateArg) {
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
+}
