461f0947dbad561c1d87e8951686a478579ac2f3
t2-g <89742029+t2-g@users.noreply.github.com>
2022-07-20 20:28:07 +0530

    images and html

---
+++ app.js
@@ -0,0 +1,3 @@
+007e5acd804e8400e57c455fd7d22383
+
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
--- main.js
+++
@@ -1,3 +0,0 @@
-007e5acd804e8400e57c455fd7d22383
-
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
